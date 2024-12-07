3afb321015c8707e4e4bf7a2a4edb324c648f169
FR-AKITO <varshakoli638@gmail.com>
2024-12-07 15:49:32 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,7 +1,7 @@
 # IF YOU CAN'T USE ENV VARIABLES FILL EVERYTHING HERE
 
 VAR_SESSION = None
-VAR_API_ID = "10187126"
-VAR_API_HASH = "ff197c0d23d7fe54c89b44ed092c1752"
+VAR_API_ID = "23241529"
+VAR_API_HASH = "f413b120fda101d2b833f132db2dbf54"
 VAR_MONGO_DB_URI = "mongodb+srv://Wine_Music:red_wine55@cluster0.hbuxm0l.mongodb.net/?retryWrites=true&w=majority"
-VAR_REPO_URL = "https://github.com/Otazuki004/SophiaUB/" # Change to your fork repo
+VAR_REPO_URL = "https://github.com/FR-AKITO/UB/" # Change to your fork repo
